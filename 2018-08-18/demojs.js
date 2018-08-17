5398f0d9da2f26c9df99c588e852ae0eb3883ec6
hoanvudn94 <41957065+hoanvudn94@users.noreply.github.com>
2018-08-18 01:23:49 +0700

    created this is buton

---
+++ 2018-08-18/demojs.js
@@ -0,0 +1,20 @@
+var a = 0;
+var Image=document.getElementById('Image')
+function myFunction(){
+    
+
+  
+    if(a==0){
+      
+        document.getElementById("Image").innerHTML = "src=./pic_buloff.gif";
+         
+         
+    }
+    else
+    {
+       Image.src='./pic_buloff.gif';
+        
+    }
+
+}
+
